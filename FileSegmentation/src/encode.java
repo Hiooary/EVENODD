1955d1745e65a639e8e924f52193a6b61d8f1be6
Hiooary <767782820@qq.com>
2017-03-19 00:02:54 +0800

    edit

--- FileSegmentation/src/encode.java
+++ FileSegmentation/src/encode.java
@@ -7,58 +7,10 @@ public class encode extends fileMatrix{
  * 编码
  */	
 @SuppressWarnings("unused")
-public encode(String filePath,int bufSize){
-		
-		//分块
-//		long length=getFileLength(filePath);
-//		long block_num = 0;
-//		int stripe_index,offset;
-//		int i;
-//		
-//		char[] readBufs=new char[K];
-//		//String readBufs=new String(a);
-//		
-//		char[] writeBufs=new char[R];
-//		
-//		if(length % BLOCK_SIZE != 0)
-//		{
-//			block_num=length / BLOCK_SIZE + 1;
-//		}
-//
-//		long stripe_num = 0;
-//		if(block_num % K != 0)
-//		{
-//			stripe_num=block_num / K + 1;
-//		}
-//		
-//		//分配缓冲区
-//		char[] buffer=new char[(K+R)* bufSize];
-//		
-////		for(i=0;i<K;i++)
-////			readBufs[i] = buffer + i * bufSize;
-////		for(i=0;i<R;i++)
-////			writeBufs[i] = buffer + (K+i) * bufSize;
-//		
-//		//对每一个Stripe，生成校验块
-//		for(stripe_index = 0;stripe_index<stripe_num;stripe_index++)
-//		{
-//			//建立系统和校验数据块对应的流
-//			for(i=0;i<(K+R);i++)
-//			{
-//				if(i<K)
-//				{
-//					offset=(int) ((stripe_index * K + i ) * BLOCK_SIZE);
-//					if(offset < length)
-//					{
-//						
-//					}
-//				}
-//			}
-//		}
-//		
-		
-		
-	}
+public static void encode(int[][][] tempMemory)
+{
+
+}
 	
 @SuppressWarnings("null")
 public static void main(String[] args) throws IOException
@@ -73,39 +25,47 @@ public static void main(String[] args) throws IOException
 	    	count= len / (M-1) + 1;
 	    //System.out.print(count);
 	    
-	    int[][][] tempMemory=null;//将数据划分为矩阵
+	    int[][][] tempMemory=new int[count][M-1][M];//将数据划分为矩阵
 	    int t=0;
-	    //while(t < len){
-		    for(int k=0;k < count;k++)
+		for(int k=0;k < count;k++)
+		{
+		    for(int i=0;i<M-1;i++)
 		    {
-			    for(int i=0;i<M-1;i++)
-				    {
-				    	for(int j=0;j<M;j++)
-				    	{
-				    		tempMemory[k][i][j]=buffer[i][t];
-				    	}
-				    	t++;
-				    }
-		    }
-	   // }
+		    	for(int j=0;j<M;j++)
+		    	{
+		    		//System.out.print(buffer[i][t]);
+		    		
+		    		//对最后的一个矩阵补齐行数
+		    		if(t>=len)
+		    		{
+		    			tempMemory[k][i][j]=0;
+		    		}
+		    		else
+		    			tempMemory[k][i][j]=buffer[i][t];
+		    		t++;
+		    	}			
+			}
+		 }
 	    
+		//输出
 //	    for(int k=0;k<2;k++)
 //	    {
 //		    for(int i=0;i<M-1;i++)
 //			    {
 //			    	for(int j=0;j<M;j++)
 //			    	{
-//			    		System.out.print(tempMemory[][i][j]+" ");
+//			    		System.out.print(tempMemory[k][i][j]+" ");
 //			    	}
 //			    	System.out.print("\n");
 //			    }
 //		    System.out.print("\n");
 //	    }
-	    
+//	    
 	    
 //	     String filePath="./1.jpg";
 //	     int bufSize;
-		//encode();
+		//编码
+		encode(tempMemory);
 		//decode();
 		//merge();
 	}
